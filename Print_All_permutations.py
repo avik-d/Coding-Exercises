e642fa6ba2ad40376fbb2bf07712974064cc63ae
avik-d <36638384+avik-d@users.noreply.github.com>
2022-01-17 22:07:39 -0600

    Update Print_All_permutations.py

--- Print_All_permutations.py
+++ Print_All_permutations.py
@@ -1,1 +1,9 @@
 
+The video lecture to view: https://www.youtube.com/watch?v=YK78FU5Ffjw&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=56&t=0s&ab_channel=takeUforward
+    
+    How does this work?
+    1. We create a map like data structure (freq) that helps us in picking what can be picked and is not already picked
+    2. Then we pick that element and recur
+    3. When "ds" becomes length of string, we add to result
+    4. We rmeove the last element from recursion stack
+    
