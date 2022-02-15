083b3441e8dc68fd4818891e33ff2f84268f71f9
avik-d <36638384+avik-d@users.noreply.github.com>
2022-02-14 23:52:01 -0600

    Update Search in sorted rotated array.py

--- Search in sorted rotated array.py
+++ Search in sorted rotated array.py
@@ -4,12 +4,10 @@
 #1) Find middle point mid = (l + h)/2
 #2) If key is present at middle point, return mid.
 #3) Else If arr[l..mid] is sorted
-    #a) If key to be searched lies in range from arr[l]
-       #to arr[mid], recur for arr[l..mid].
+    #a) If key to be searched lies in range from arr[l] to arr[mid], recur for arr[l..mid].
     #b) Else recur for arr[mid+1..h]
 #4) Else (arr[mid+1..h] must be sorted)
-    #a) If key to be searched lies in range from arr[mid+1]
-    #   to arr[h], recur for arr[mid+1..h].
+    #a) If key to be searched lies in range from arr[mid+1] to arr[h], recur for arr[mid+1..h].
     #b) Else recur for arr[l..mid]  
 
 class Solution:
